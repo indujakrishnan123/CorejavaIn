25444b844000d0202d4387e182a5d5c521519be8
indujakrishnan123 <113790885+indujakrishnan123@users.noreply.github.com>
2022-11-10 10:28:33 +0530

    coreIn

---
+++ corejava/src/com/collection/PQLifo.java
@@ -0,0 +1,10 @@
+package com.collection;
+
+import java.util.PriorityQueue;
+
+public class PQLifo {
+public static void main(String[] args) {
+	PriorityQueue pr=new PriorityQueue();
+	//pr.
+}
+}
---
+++ corejava/src/excelread/Excelread1Exam.java
@@ -0,0 +1,5 @@
+package excelread;
+
+public class Excelread1Exam {
+
+}
